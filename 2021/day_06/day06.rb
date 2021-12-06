d16a4a379671abf52f7902b36961c2745fb53fad
Jose Luis Cambero <joseluis.cambero@sngular.com>
2021-12-06 15:06:33 +0100

    2021 day 6

---
+++ 2021/day_06/day06.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def fish_after_days(fish, days)
+  days.times.inject(fish) { |f| f.rotate.tap { _1[6] += _1[8] } }
+end
+
+initials = open('input').read.split(',').map(&:to_i).tally
+fish = Array.new(9) { initials[_1].to_i }
+
+puts fish_after_days(fish, 80).sum == 390_011
+puts fish_after_days(fish, 256).sum == 1_746_710_169_834
